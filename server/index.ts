72f59e20c3dc4b1f945b77f4f34e3f3a8a13bef6
Hahnbee Lee <55263191+hahnbeelee@users.noreply.github.com>
2023-01-11 21:15:02 -0800

    express index.ts

---
+++ server/index.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json({ limit: "5mb" }));
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"));
+}
+app.set("trust proxy", 1);
+
+app.listen(PORT, () => {
+  console.log(`Listening at PORT ${PORT}`);
+});
